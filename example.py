1c7212339ba15f8ad84d4adc70c77356451bd024
Mathis Weber <mathis.c.weber@web.de>
2023-04-08 15:08:55 +0200

    chore: format

--- example.py
+++ example.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 from truthtables import Statement, Formatter, Formatting
 
+
 def main():
     statements = [
         Statement("not A or B"),
@@ -9,6 +10,6 @@ def main():
     f = Formatter(statements, mode=Formatting.LATEX)
     print(f.format_table())
 
+
 if __name__ == "__main__":
     main()
-
