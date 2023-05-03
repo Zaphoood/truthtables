5b7b7f3cedadd19fc9052f99e46230ce43fadc34
Mathis Weber <mathis.c.weber@web.de>
2023-05-03 10:56:01 +0200

    refactor: move constants to separate file

--- truthtables.py
+++ truthtables.py
@@ -1,74 +1,25 @@
 from __future__ import annotations
 
-from enum import Enum, IntEnum, auto
 from string import ascii_uppercase
-from typing import Callable, Optional, Set
+from typing import Optional, Set
 
+from common import (
+    BINARY_OPERATORS,
+    FALSE_STR,
+    LATEX_COLUMN_DELIM,
+    LATEX_HLINE,
+    LATEX_INDENT,
+    LATEX_TABLE_EPILOGUE,
+    LATEX_TABLE_PRELUDE,
+    LATEX_WRAP_CHAR,
+    OPERATOR_TO_STR,
+    TRUE_STR,
+    UNARY_OPERATORS,
+    Formatting,
+    Operator,
+    operator_macros,
+)
 from util import split_tokens, table_to_str, unwrap_parentheses
-
-
-class Formatting(Enum):
-    HUMAN = auto()
-    LATEX = auto()
-
-
-# Ordered by precedence (highest to lowest)
-class Operator(IntEnum):
-    NONE = auto()
-    NOT = auto()
-    AND = auto()
-    OR = auto()
-    IMPLIES = auto()
-    EQUIVALENT = auto()
-
-
-UNARY_OPERATORS: dict[Operator, Callable[[bool], bool]] = {
-    Operator.NONE: lambda a: a,
-    Operator.NOT: lambda a: not a,
-}
-
-BINARY_OPERATORS: dict[Operator, Callable[[bool, bool], bool]] = {
-    Operator.AND: lambda a, b: a and b,
-    Operator.OR: lambda a, b: a or b,
-    Operator.IMPLIES: lambda a, b: (not a) or b,
-    Operator.EQUIVALENT: lambda a, b: a == b,
-}
-
-
-OPERATOR_TO_STR: dict[Formatting, dict[Operator, str]] = {
-    Formatting.HUMAN: {
-        Operator.NOT: "¬",
-        Operator.AND: "∧",
-        Operator.OR: "∨",
-        Operator.IMPLIES: "⇒",
-        Operator.EQUIVALENT: "⇔",
-    },
-    Formatting.LATEX: {
-        Operator.NOT: "\\lnot",
-        Operator.AND: "\\land",
-        Operator.OR: "\\lor",
-        Operator.IMPLIES: "\\Rightarrow",
-        Operator.EQUIVALENT: "\\Leftrightarrow",
-    },
-}
-LATEX_TABLE_PRELUDE = "\\begin{{tabular}}{{ {columns} }}"
-LATEX_TABLE_EPILOGUE = "\\end{tabular}"
-LATEX_COLUMN_DELIM = " & "
-LATEX_INDENT = "    "
-LATEX_HLINE = LATEX_INDENT + " \\\\ \\hline"
-LATEX_WRAP_CHAR = "$"
-TRUE_STR = "w"
-FALSE_STR = "f"
-
-operator_macros = {
-    "not": Operator.NOT,
-    "and": Operator.AND,
-    "or": Operator.OR,
-    "eq": Operator.EQUIVALENT,
-    "<=>": Operator.EQUIVALENT,
-    "impl": Operator.IMPLIES,
-    "=>": Operator.IMPLIES,
-}
 
 
 class MalformedExpressionError(Exception):
